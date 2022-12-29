ad8d21b915b150d5224aa46b3e1157a4b6435d65
Artur M <artyrillo@yandex.ru>
2022-12-29 14:17:57 +0300

    tg bot

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,48 +1,11 @@
 package org.example;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-
-import java.io.FileOutputStream;
-import java.io.IOException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Main {
 
-    public static final String URI = "https://api.nasa.gov/planetary/apod?api_key=AzdkXwVUj2azyVYb7fTEUViwEa5qcGBPaOnpP6do";
+    public static void main(String[] args) throws TelegramApiException {
 
-    public static final ObjectMapper mapper = new ObjectMapper();
-
-    public static void main(String[] args) throws IOException {
-
-        CloseableHttpClient httpClient = HttpClientBuilder.create()
-                .setDefaultRequestConfig(RequestConfig.custom()
-                        .setConnectTimeout(5000)
-                        .setSocketTimeout(30000)
-                        .setRedirectsEnabled(false)
-                        .build())
-                .build();
-
-        CloseableHttpResponse httpResponse = httpClient.execute(new HttpGet(URI));
-
-        NasaObject nasaObject = mapper.readValue(httpResponse.getEntity().getContent(), NasaObject.class);
-        System.out.println(nasaObject);
-
-        CloseableHttpResponse pictureResponse = httpClient.execute(new HttpGet(nasaObject.getUrl()));
-
-        String[] arr = nasaObject.getUrl().split("/");
-        String fileName = arr[arr.length - 1];
-        System.out.println("Имя файла " + fileName);
-
-        HttpEntity entity = pictureResponse.getEntity();
-        if (entity != null) {
-            FileOutputStream fos = new FileOutputStream(fileName);
-            entity.writeTo(fos);
-            fos.close();
-        }
+        new TelegramBot();
     }
 }
